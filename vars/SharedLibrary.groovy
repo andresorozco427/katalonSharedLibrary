65fe35ff776e277097b37b82c2f07e065e95456f
Andres Orozco Muñoz <andres.orozco@ceiba.com.co>
2019-12-02 18:28:43 -0500

    Version 3

--- vars/SharedLibrary.groovy
+++ vars/SharedLibrary.groovy
@@ -1,6 +1,7 @@
-import constantes.ConstantesKatalon
-
 def call(Map params = [:]) {
+
+    boolean esTestSuite = params.get('testSuite')
+    boolean esTestCollection = params.get('testCollection')
 
     // obligatorios
     def rutaProyecto = params.get('rutaProyecto')
@@ -13,16 +14,35 @@ def call(Map params = [:]) {
     //def pathReporte = params.get('pathReporte') ?: ConstantesKatalon.RUTA_REPORTE_POR_DEFECTO + "\\${BUILD_NUMBER}"
     def cantidadReintentos = params.get('cantidadReintentos') ?: 0
     def reintentar = params.get('reintentar') ?: "false"
-    def perfil = params.get('perfil') ?: ConstantesKatalon.PERFIL_DEFECTO_KATALON
+    def perfil = params.get('perfil') ?: "default"
+    
     
     def scriptKatalon = "${KATALON_HOME}\\katalon -noSplash -runMode=console " + " -projectPath=\"" + rutaProyecto + "\""
     scriptKatalon = scriptKatalon + " -retry=${cantidadReintentos} -retryFailedTestCases=${reintentar} -reportFolder=\"" + pathReporte + "\""
     scriptKatalon = scriptKatalon + " -reportFileName=\"${BUILD_NUMBER}-KatalonResult\""
+ 
     scriptKatalon = scriptKatalon + " -testSuitePath=\"Test Suites/" + testSuite + "\""
     scriptKatalon = scriptKatalon + " -browserType=\"" + browser + "\""
     
     scriptKatalon = scriptKatalon + " -executionProfile=\"" + perfil + "\""
-    scriptKatalon = scriptKatalon + " --config -proxy.option=MANUAL_CONFIG -proxy.server.type=HTTPS -proxy.server.address=\"http://proxy.epm.com.co\" -proxy.server.port=\"8080\""
 
     bat scriptKatalon
+}
+
+
+def checkout_repository(Map params = [:])
+{   
+    def urlrepositorio = params.urlrepositorio
+    def branche = params.branche ?: "master"
+
+    echo "...............CHECKOUT..............."
+			  checkout([  
+				  	$class: 'GitSCM',
+			 	  	branches: [ [ name: '*/${branche}' ] ],
+			 	  	doGenerateSubmoduleConfigurations: false,
+			 	    extensions: [],
+                    submoduleCfg: [],
+                     url: params.urlrepositorio])	   
+							
+			}
 }
